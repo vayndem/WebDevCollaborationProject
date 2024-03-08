c7c652587af5fc214204a1cc582e324f643104ed
anggernuramin <anggern514@gmail.com>
2024-03-08 13:33:34 +0700

    improvment: layout dashboard

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,13 @@
-const login = document.querySelector("#login");
-const username = document.querySelector("#username");
-const password = document.querySelector("#password");
-const showPassword = document.querySelector("#show-password");
+// $(document).ready(function () {
+//   $("#sidebarCollapse").on("click", function () {
+//     $("#sidebar").toggleClass("active");
+//   });
+// });
 
-// hidden alert error
-const alertError = document.querySelector("#alert-error");
-alertError.style.display = "none";
+const sideBarCollapse = document.getElementById("sidebarCollapse");
+const sidebar = document.getElementById("sidebar");
 
-// show password
-showPassword.addEventListener("click", () => {
-  if (password.type === "password") {
-    password.type = "text";
-    showPassword.innerHTML = `<i class="bi bi-eye-slash fs-4"></i>`;
-  } else {
-    password.type = "password";
-    showPassword.innerHTML = ` <i
-    class="bi bi-eye fs-4"
-  ></i>`;
-  }
-});
-
-// event submit login
-login.addEventListener("submit", (e) => {
-  e.preventDefault();
-  //   validasi login
-  if (username.value === "admin" && password.value === "admin") {
-    window.location.href = "../index.html";
-    alertError.style.display = "none";
-  } else {
-    alertError.style.display = "block";
-  }
+sideBarCollapse.addEventListener("click", () => {
+  console.log("yese");
+  sidebar.classList.toggle("active");
 });
